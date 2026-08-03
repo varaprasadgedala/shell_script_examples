b6a39a9ba55767dc10b725fe5d779edbdadfca5f
prasad <varaprasadg870@gmail.com>
2026-08-03 02:58:53 +0000

    second commit

---
+++ examples/create_twofiles_inside_folder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This shell script creates two files inside a folder
+
+# Create a folder
+mkdir test
+
+# change directory
+cd test
+
+#create two files inside this folder
+
+touch first_file second_file
